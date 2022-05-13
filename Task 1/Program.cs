fd1759fbaaf0edaa490ec592d4f425f1d53f3e0f
«Mikhail» <kopfgolova@gmail.com>
2022-05-13 10:29:49 +0300

    add Task 1 dz

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿//Показать четные числа от 1 до N
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+for (int i = 1; i < number; i++)
+{
+    if (i % 2 == 0) Console.Write($"{i}, ");
+}
