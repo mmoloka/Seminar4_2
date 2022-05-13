02d9cf8742c6ba74e5de7e9c8440b06f4cbb090e
«Mikhail» <kopfgolova@gmail.com>
2022-05-13 11:15:37 +0300

    add Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,5 @@
+﻿/* Напишите программу, которая выводит на экран случайное 
+трехзначное число и удалет вторую цифру в нем*/
+int number = new Random().Next(100,1000);
+string str = number.ToString();
+Console.WriteLine($"{number} -> {str[0]}{str[2]}");
