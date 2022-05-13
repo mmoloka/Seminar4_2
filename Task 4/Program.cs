78966e6ad0d204a7316f7c483fe189b55db8c79e
«Mikhail» <kopfgolova@gmail.com>
2022-05-13 13:14:55 +0300

    add Task 4 new version

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -7,11 +7,7 @@
 
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
-if((num%7==0)&&(num%23==0))
-{
+if ((num % 7 == 0) && (num % 23 == 0))
     Console.WriteLine($"Число {num} кратно 7 и 23");
-}
 else
-{
     Console.WriteLine($"Число {num} не кратно 7 и 23 одновременно");
-}
